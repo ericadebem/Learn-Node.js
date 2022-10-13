0651ea5c11d75f19b8858ff722f23bbdcc0dce11
ericadebem <110907550+ericadebem@users.noreply.github.com>
2022-10-13 20:00:16 +0200

    second test check

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,17 @@
 const pegaArquivo = require('../index');
 
-test('deve ser uma função', () => {
-  expect(typeof pegaArquivo).toBe('function');
-});
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo ::', () => {
+  it('deve ser uma função', () => {
+    expect(typeof pegaArquivo).toBe('function');
+  })
+  it('deve retornar array com resultados', async() => {
+    const resultado = await pegaArquivo('/Users/ericadebem/Desktop/2299-lib-nodejs-markdown-aula-1/test/arquivos/texto1.md')
+    expect(resultado).toEqual(arrayResult)
+  })
+})
